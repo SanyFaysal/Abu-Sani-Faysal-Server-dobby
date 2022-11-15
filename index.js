9376d08d7a076531cbfe1aa615e271024cdfee7d
Abu Sani Faysal <97028772+SanyFaysal@users.noreply.github.com>
2022-11-15 15:01:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+
+const app = require('./app');
+
+try {
+  // Connect to the MongoDB cluster
+  mongoose.connect(
+    process.env.DB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log(`Mongoose is connected`.blue.bold)
+  );
+} catch (e) {
+  console.log(`could not connect`.red.bold);
+}
+
+// server
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`.yellow.bold);
+});
